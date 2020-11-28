5b2341fa1b4a7cb26f110f0cb7743148929933d4
mahartariq <mahartariq499@gmail.com>
2020-11-28 14:31:27 +0500

    changes for a tag

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './Navbar.css'
 
-function Nav(){
-    return(
+function Nav() {
+    return (
         <div>
             <ul>
-                <li><a href="#">HTML</a></li>
-                <li><a  href="#">CSS</a></li>
-                <li><a href="#">Javascript</a></li>
-                <li><a href="#">ReactJS</a></li>
-                <li><a href="#">MongoDB</a></li>
-                <li><a href="#">Express.js</a></li>
-                <li><a href="#">Node.js</a></li>
-                <li><a href="#">Git</a></li>
-                <li><a href="#">Python</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
+                <li><a href="https://github.com/mahartariq">HTML</a></li>
+                <li><a href="https://github.com/mahartariq">CSS</a></li>
+                <li><a href="https://github.com/mahartariq">Javascript</a></li>
+                <li><a href="https://github.com/mahartariq">ReactJS</a></li>
+                <li><a href="https://github.com/mahartariq">MongoDB</a></li>
+                <li><a href="https://github.com/mahartariq">Express.js</a></li>
+                <li><a href="https://github.com/mahartariq">Node.js</a></li>
+                <li><a href="https://github.com/mahartariq">Git</a></li>
+                <li><a href="https://github.com/mahartariq">Python</a></li>
+                <li><a href="https://github.com/mahartariq">About us</a></li>
+                <li><a href="https://github.com/mahartariq">Contact us</a></li>
             </ul>
         </div>
     )
