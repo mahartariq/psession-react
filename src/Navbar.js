2e6d2e08b68117f88ec4922e49e3fdc4c65d976a
mahartariq <mahartariq499@gmail.com>
2020-11-28 14:05:09 +0500

    your turn done from daveceddia

---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css'
+
+function Nav(){
+    return(
+        <div>
+            <ul>
+                <li><a>HTML</a></li>
+                <li><a>CSS</a></li>
+                <li><a>Javascript</a></li>
+                <li><a>ReactJS</a></li>
+                <li><a>MongoDB</a></li>
+                <li><a>Express.js</a></li>
+                <li><a>Node.js</a></li>
+                <li><a>Git</a></li>
+                <li><a>Python</a></li>
+                <li><a>About us</a></li>
+                <li><a>Contact us</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Nav from './Navbar'
+
+function Hi(){
+  return <div>
+    
+    <h2 className="c1">Front-End Developer </h2>
+    <h2 className="c1">Back-End Developer  </h2>
+    <h2 className="c2">Full Stack Developer</h2>
+    <h2 className="c2">Mern Stack Developer</h2>
+    <h2 className="c2">JAM Stack Developer </h2>
+    <h2 className="c1">Mean Stack Developer</h2>
+    <h2 className="c1">Mevn Stack Developer</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Javascript</li>
+      <li>React</li>
+    </ul>
+    
+    </div>
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <Nav/>
+  <Hi></Hi>
+  </div>
+  ,document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
